c148f750b57483fe99afeec3845edc9520e7c027
oickle <oycell@aol.com>
2025-07-20 14:48:18 -0400

    style: clean up code and add comments

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import type { MouseEventHandler } from "svelte/elements";
 
     let {
         width,
@@ -26,46 +25,57 @@
     onMount(() => {
         context = canvas.getContext("2d") as CanvasRenderingContext2D;
 
+        // Initialize brush attributes to current brush values
         context.strokeStyle = brushColor;
         context.lineWidth = brushWidth;
 
+        // Initialize canvas' offset
         setOffset();
     });
 
+    // Ensures that mouse pointer is correctly offset to within the Canvas element
     function setOffset() {
         const rect = canvas.getBoundingClientRect();
         offsetX = rect.x;
         offsetY = rect.y;
     }
 
-    function startDraw(x: number, y: number) {
+    // Toggle drawing on and grab line stroke's starting position
+    function startDraw(event: MouseEvent) {
         isDrawing = true;
 
-        prevX = x - offsetX;
-        prevY = y - offsetY;
+        prevX = event.x - offsetX;
+        prevY = event.y - offsetY;
     }
 
-    function draw(event) {
+    // Handles drawing as mouse moves around canvas
+    function draw(event: MouseEvent) {
+        // Only draw line strokes if mouse is held down
         if (isDrawing) {
+            // Grab current mouse position with consideration for offset
             const x = event.x - offsetX;
             const y = event.y - offsetY;
 
+            // Draw a line stroke from the previous mouse position to the current mouse position
             context.beginPath();
             context.moveTo(prevX, prevY);
             context.lineTo(x, y);
             context.closePath();
             context.stroke();
 
+            // Update previous mouse position
             prevX = x;
             prevY = y;
         }
     }
 
+    // Toggle drawing off when mouse is released
     function endDraw() {
         isDrawing = false;
     }
 </script>
 
+<!-- Update offset whenever the window's size is changed -->
 <svelte:window onresize={setOffset} />
 
 <canvas
