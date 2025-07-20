24069ad28053456d951a72f392fb3baa4cdbc338
oickle <oycell@aol.com>
2025-07-20 16:00:54 -0400

    feat: add basic undo and redo

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -16,11 +16,21 @@
     let canvas: HTMLCanvasElement;
     let context: CanvasRenderingContext2D;
 
+    // Variables relating to active drawing
     let isDrawing = false;
     let offsetX: number;
     let offsetY: number;
-    let prevX: number;
-    let prevY: number;
+    let prevMouseX: number;
+    let prevMouseY: number;
+
+    interface Stroke {
+        x: number;
+        y: number;
+    }
+
+    let undoPointer: number = 0;
+    let currentStroke: Array<Stroke> = [];
+    let strokes: Array<Array<Stroke>> = [];
 
     onMount(() => {
         context = canvas.getContext("2d") as CanvasRenderingContext2D;
@@ -40,50 +50,129 @@
         offsetY = rect.y;
     }
 
+    // Draw a line stroke from the previous mouse position to the current mouse position
+    function draw(prevX: number, prevY: number, x: number, y: number) {
+        context.beginPath();
+        context.moveTo(prevX, prevY);
+        context.lineTo(x, y);
+        context.closePath();
+        context.stroke();
+    }
+
     // Toggle drawing on and grab line stroke's starting position
-    function startDraw(event: MouseEvent) {
+    function startMouseDraw(event: MouseEvent) {
+        //
+        // If undo point is not at end of strokes array, wipe all data after it
+        //
+
         isDrawing = true;
 
-        prevX = event.x - offsetX;
-        prevY = event.y - offsetY;
+        prevMouseX = event.x - offsetX;
+        prevMouseY = event.y - offsetY;
+
+        currentStroke.push({ x: prevMouseX, y: prevMouseY });
     }
 
     // Handles drawing as mouse moves around canvas
-    function draw(event: MouseEvent) {
+    function mouseDraw(event: MouseEvent) {
         // Only draw line strokes if mouse is held down
         if (isDrawing) {
             // Grab current mouse position with consideration for offset
             const x = event.x - offsetX;
             const y = event.y - offsetY;
 
-            // Draw a line stroke from the previous mouse position to the current mouse position
-            context.beginPath();
-            context.moveTo(prevX, prevY);
-            context.lineTo(x, y);
-            context.closePath();
-            context.stroke();
+            // Draw out line from previous mouse position to current mouse position
+            draw(prevMouseX, prevMouseY, x, y);
 
             // Update previous mouse position
-            prevX = x;
-            prevY = y;
+            prevMouseX = x;
+            prevMouseY = y;
+
+            // Append the new points to the array of points form the current stroke
+            currentStroke.push({ x, y });
         }
     }
 
     // Toggle drawing off when mouse is released
     function endDraw() {
         isDrawing = false;
+
+        // Append the current stroke to the array of all strokes and reset current stroke array for use with next stroke
+        strokes.push(currentStroke);
+        currentStroke = [];
+
+        // Move undo pointer to the end of strokes
+        undoPointer = strokes.length;
+    }
+
+    // Move undo pointer back one (if possible)
+    function undo() {
+        if (undoPointer) --undoPointer;
+        render();
+    }
+
+    // Move undo pointer forward one (if possible)
+    function redo() {
+        if (undoPointer < strokes.length) ++undoPointer;
+        render();
+    }
+
+    // Takes all currently drawn lines and places them onto the screen
+    function render() {
+        // Wipe all strokes on canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Render out every single brush stroke
+        for (let i = 0; i < undoPointer; i++) {
+            const stroke = strokes[i];
+
+            // Grab initial brush stroke point
+            let prevX = stroke[0].x;
+            let prevY = stroke[0].y;
+
+            // Render out every single point for the current stroke
+            for (let j = 1; j < stroke.length; j++) {
+                const x = stroke[j].x;
+                const y = stroke[j].y;
+
+                // Draw a line stroke from the previous mouse position to the current mouse position
+                draw(prevX, prevY, x, y);
+
+                prevX = x;
+                prevY = y;
+            }
+        }
+    }
+
+    // Handles keyboard shortcut for the Canvas
+    function onkeydown(event: KeyboardEvent) {
+        if (event.ctrlKey) {
+            switch (event.key.toLowerCase()) {
+                case "z":
+                    // Allows for undo and redo with Ctrl+Z and Ctrl+Shift+Z
+                    if (!event.shiftKey) undo();
+                    else redo();
+
+                    break;
+                case "y":
+                    // Allows for redo functionality with Ctrl+Y
+                    redo();
+
+                    break;
+            }
+        }
     }
 </script>
 
 <!-- Update offset whenever the window's size is changed -->
-<svelte:window onresize={setOffset} />
+<svelte:window onresize={setOffset} {onkeydown} />
 
 <canvas
     {width}
     {height}
     bind:this={canvas}
-    onmousedown={startDraw}
-    onmousemove={draw}
+    onmousedown={startMouseDraw}
+    onmousemove={mouseDraw}
     onmouseup={endDraw}
     class="bg-white rounded-md"
 >
