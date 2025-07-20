54b7e8ef6d17669ccffb6d9dfa23848579073cc4
oickle <oycell@aol.com>
2025-07-20 12:03:41 -0400

    test: customize header to ensure project is set up properly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-2xl bg-black text-white min-w-120 text-center py-3">
+    Welcome to flopnote!
+</h1>
